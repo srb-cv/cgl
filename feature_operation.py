324292bca25084bf13dafb414f6b65527be4a84c
mindgarage05 <s_varshney16@cs.uni-kl.de>
2019-01-05 01:35:49 +0100

    Remove deprecated volatile pytorch

--- feature_operation.py
+++ feature_operation.py
@@ -63,12 +63,13 @@ class FeatureOperator:
             input.div_(255.0 * 0.224)
             if settings.GPU:
                 input = input.cuda()
-            input_var = V(input,volatile=True)
-            logit = model.forward(input_var)
-            while np.isnan(logit.data.max()):
-                print("nan") #which I have no idea why it will happen
-                del features_blobs[:]
-                logit = model.forward(input_var)
+            #input_var = V(input,volatile=True)
+            with torch.no_grad():
+                logit = model.forward(input)
+                while np.isnan(logit.cpu().data.max()):
+                    print("nan") #which I have no idea why it will happen
+                    del features_blobs[:]
+                    logit = model.forward(input)
             if maxfeatures[0] is None:
                 # initialize the feature variable
                 for i, feat_batch in enumerate(features_blobs):
