cfd93da9082ada588872110587076ea5e09baa3c
Saurabh Varshneya <s_varshney16@informatik.uni-kl.de>
2019-09-22 22:15:20 +0200

    Add entries to settings.py to consider toy datset

--- settings.py
+++ settings.py
@@ -2,14 +2,15 @@
 GPU = True                                  # running on GPU is highly suggested
 TEST_MODE = False                           # turning on the testmode means the code will run on a small dataset.
 CLEAN = True                               # set to "True" if you want to clean the temporary large files after generating result
-MODEL = 'alexnet'                          # model arch: resnet18, alexnet, resnet50, densenet161
-DATASET = 'places50'                       # model trained on: places365 or imagenet
+MODEL = 'toy_data_model'                          # model arch: resnet18, alexnet, resnet50, densenet161, toy_data_model
+DATASET = 'toy_data'                       # model trained on: places365 or imagenet or places50 or toy_data
 QUANTILE = 0.005                            # the threshold used for activation
 SEG_THRESHOLD = 0.04                        # the threshold used for visualization
 SCORE_THRESHOLD = 0.04                      # the threshold used for IoU score (in HTML file)
 TOPN = 10                                   # to show top N image with highest activation for each unit
 PARALLEL = 1                                # how many process is used for tallying (Experiments show that 1 is the fastest)
-CATAGORIES = ["object", "part","scene","texture","color"] # concept categories that are chosen to detect: "object", "part", "scene", "material", "texture", "color"
+#CATAGORIES = ["object", "part","scene","texture","color"] # concept categories that are chosen to detect: "object", "part", "scene", "material", "texture", "color"
+CATAGORIES = ["color","shape","shape_color"]
 OUTPUT_FOLDER = "result/pytorch_"+MODEL+"_"+DATASET # result will be stored in this folder
 
 ########### sub settings ###########
@@ -25,12 +26,18 @@ OUTPUT_FOLDER = "result/pytorch_"+MODEL+"_"+DATASET # result will be stored in t
 # TALLY_BATCH_SIZE: batch size used in tallying
 # INDEX_FILE: if you turn on the TEST_MODE, actually you should provide this file on your own
 
-if MODEL != 'alexnet':
-    DATA_DIRECTORY = '/scratch/data/broden1_224'
-    IMG_SIZE = 224
-else:
+if MODEL == 'alexnet':
     DATA_DIRECTORY = '/scratch/data/broden1_227'
+    IMG_SIZE = 224
+elif MODEL == 'toy_data_model':
+    DATA_DIRECTORY = '/scratch/data/synthetic_labels_v2'
+    IMG_SIZE = 64
+
+else:
+    DATA_DIRECTORY = '/scratch/data/broden1_224'
     IMG_SIZE = 227
+
+
 
 if DATASET == 'places365':
     NUM_CLASSES = 365
@@ -38,6 +45,8 @@ elif DATASET == 'imagenet':
     NUM_CLASSES = 1000
 elif DATASET == 'places50' or DATASET == 'places50_reduced':
     NUM_CLASSES = 50
+elif DATASET == 'toy_data':
+    NUM_CLASSES = 45
 if MODEL == 'resnet18':
     FEATURE_NAMES = ['layer4']
     if DATASET == 'places365':
@@ -61,11 +70,20 @@ elif MODEL == 'resnet50':
 elif MODEL == 'alexnet':
     FEATURE_NAMES = ['conv3','conv4', 'conv5']
     if DATASET == 'places365' or DATASET == 'places50':
-        FOLDER_NAME= 'l_2_1_lr0.01_wd0_pen0.005_act0_spa0.01_b256_c50'
+        FOLDER_NAME= 'l_2_1_lr0.01_wd0_pen0.005_act0_spa0.01_b256_c50_id49'
         MODEL_FILE = '/home/mindgarage05/magus/back_up_thesis/saved_models/mg03_models_runs/' \
                      'zoo/'+FOLDER_NAME+'/alexnet_best.pth.tar'
         MODEL_PARALLEL = True
         OUTPUT_FOLDER = OUTPUT_FOLDER+"_"+FOLDER_NAME
+
+elif MODEL == 'toy_data_model':
+    FEATURE_NAMES = ['conv1']
+    if DATASET == 'toy_data':
+        #FOLDER_NAME= 'l_2_1_lr0.01_wd0_pen0.005_act0_spa0.01_b256_c50_id49'
+        MODEL_FILE = '/home/mindgarage05/magus/projects/netdissect-lite/Places365/custom_model_1_best.pth.tar'
+        MODEL_PARALLEL = True
+        OUTPUT_FOLDER = OUTPUT_FOLDER+"_"+"custom_data_result"
+
 
 if TEST_MODE:
     WORKERS = 1
@@ -80,4 +98,3 @@ else:
     TALLY_BATCH_SIZE = 16
     TALLY_AHEAD = 4
     INDEX_FILE = 'index.csv'
-        #MODEL_FILE = '/home/mindgarage05/magus/projects/paper_implementations/NetDissect-Lite/Places365/l1.33_penalty0.01_lr0.01_wd0/alexnet_best.pth.tar'
