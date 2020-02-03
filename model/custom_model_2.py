5f8b1db53f2956c2f52f627fc074e755f6f860f5
Saurabh Varshneya <s_varshney16@informatik.uni-kl.de>
2020-02-03 16:48:18 +0100

    change custom model to add more filters

--- model/custom_model_2.py
+++ model/custom_model_2.py
@@ -1,3 +1,6 @@
+'''
+Creates a custom model with two CNN layers
+'''
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -5,21 +8,21 @@ class CustomModel2(nn.Module):
 
     def __init__(self, num_classes=1000):
         super(CustomModel2, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(128, num_classes)
+        self.conv1 = nn.Conv2d(3, 128, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(256, num_classes)
         self.dropout = nn.Dropout()
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
     def forward(self, x):
-        x = self.conv1(x) #bx3x64x64 => bx64x64x64
+        x = self.conv1(x)
         conv1 = x.clone()
-        x =  F.max_pool2d(F.relu(x, inplace=True), kernel_size=2, stride=2) # => bx64x32x32
-        x = self.conv2(x) # => bx128x32x32
+        x =  F.max_pool2d(F.relu(x, inplace=True), kernel_size=2, stride=2)
+        x = self.conv2(x)
         conv2 = x.clone()
-        x = F.relu(x, inplace=True)  # => bx128x16x16
-        x = self.avgpool(x)  # bx128x1x1
-        x = x.view(x.size(0), 128)
+        x = F.relu(x, inplace=True)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), 256)
         x = self.fc1(x)
-        conv_features = [conv1, conv2]
+        conv_features = [conv2]
         return x, conv_features
